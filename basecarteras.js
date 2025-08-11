f07d00cc23e4c0eb0b11b5d0b1a43f2a2a052343
sachettis <sachetticintia3@gmail.com>
2025-08-11 12:57:47 -0500

    Add files via upload

---
+++ basecarteras.js
@@ -0,0 +1,136 @@
+import {comparaid} from './comparaid.js';
+
+export const carteras=[
+    {
+        id:1,
+        nombre:'Mochila Dora',
+        medidas: '25/35/10cm',
+        caracteristicas:'Forrada',
+        precio: '$24.000',
+        imagen: './src/assets/imagenes/dora2.jpg',
+        colores:['negro'],
+        categoria:'Mochilas'
+    },
+    {
+        id:2,
+        nombre:'Mochila Eugenia',
+        medidas: '32x25x20cm',
+        caracteristicas:'Forrada',
+        precio: '$19.000',
+        imagen: './src/assets/imagenes/eugenia2.jpg',
+        colores:['negro'],
+        categoria:'Mochilas'
+    },
+    {
+        id:3,
+        nombre:'Mochila Bruna',
+        medidas: '22x21x10cm',
+        caracteristicas:'Forrada, bolsillo externo con cierre',
+        precio: '$19.000',
+        imagen: './src/assets/imagenes/bruna2.jpg',
+        colores:['negro','blanco','rojo','suela'],
+        categoria:'Mochilas'
+    },
+    {
+        id:4,
+        nombre:'Bolso Puffer plateado cebra',
+        medidas: '32x46x10cm',
+        caracteristicas:'Forrado, correas amplias sublimadas',
+        precio: '$28.000',
+        imagen: './src/assets/imagenes/pufer2.jpg',
+        colores:['plateado'],
+        categoria:'Bolsos'
+    },
+    {
+        id:5,
+        nombre:'Bolso Pufer aqua paris',
+        medidas: '32x46x10cm',
+        caracteristicas:'Forrado, correas amplias sublimadas',
+        precio: '$28.000',
+        imagen: './src/assets/imagenes/pufer_acqua2.jpg',
+        colores:['aqua'],
+        categoria:'Bolsos'
+    },
+    {
+        id:6,
+        nombre:'Cartera Nuria negro',
+        medidas: '39x29x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela matelasse',
+        precio: '$22.000',
+        imagen: './src/assets/imagenes/nuria_negro2.jpg',
+        colores:['negro'],
+        categoria:'Carteras'
+    },
+    {
+        id:7,
+        nombre:'Cartera Matilde color maiz y bolsillo negro',
+        medidas: '32x32x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela impermeable',
+        precio: '$24.000',
+        imagen: './src/assets/imagenes/matilde2.jpg',
+        colores:['maiz'],
+        categoria:'Carteras'
+    },
+    {
+        id:8,
+        nombre:'Cartera Matilde color rosado y bolsillo negro',
+        medidas: '32x32x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela impermeable',
+        precio: '$24.000',
+        imagen: './src/assets/imagenes/matilde_rosado2.jpg',
+        colores:['rosado'],
+        categoria:'Carteras'
+    },
+    {
+        id:9,
+        nombre:'Cartera Matilde color azul y bolsillo negro',
+        medidas: '32x32x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela impermeable',
+        precio: '$24.000',
+        imagen: './src/assets/imagenes/matilde_azul2.jpg',
+        colores:['azul'],
+        categoria:'Carteras'
+    },
+    {
+        id:10,
+        nombre:'Cartera Nuria suela',
+        medidas: '39x29x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela matelasse',
+        precio: '$22.000',
+        imagen: './src/assets/imagenes/nuria_suela2.jpg',
+        colores:['suela'],
+        categoria:'Carteras'
+    },
+    {
+        id:11,
+        nombre:'Cartera Nuria rosa',
+        medidas: '39x29x10cm',
+        caracteristicas:'Forrada, cierre interno y externo, tela matelasse',
+        precio: '$22.000',
+        imagen: './src/assets/imagenes/nuria_rosa2.jpg',
+        colores:['rosa'],
+        categoria:'Carteras'
+    },
+    {
+        id:12,
+        nombre:'Cartera Felipa',
+        medidas: '',
+        caracteristicas:'',
+        precio: '$27.000',
+        imagen: './src/assets/imagenes/felipa.jpg',
+        colores:['rojo'],
+        categoria:'Carteras'
+    },
+    {
+        id:13,
+        nombre:'Bolso Stefi rojo',
+        medidas: '44x34x9cm',
+        caracteristicas:'Bolso confeccionado en cordura,bolsillo interno y externo, lavable a mano',
+        precio: '$19.000',
+        imagen: './src/assets/imagenes/stefi2.jpg',
+        colores:['rojo'],
+        categoria:'Bolsos'
+    }
+
+];
+comparaid();
